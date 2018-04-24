45ccca9a9c009f3a92587e2e8bef602e49fe95cd
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-04-24 14:43:11 -0400

    Initial Commit

---
+++ hashProg4.ps1
@@ -0,0 +1,31 @@
+﻿function Get-Fim{
+    param (
+            $directory = 'C:\sunny'
+
+    )
+
+$filehash = @{}
+Get-ChildItem -Path $directory -File -Recurse | Get-FileHash | Select-Object Path,Hash | foreach{$filehash[$_.path] = $_.hash}
+$readcsv = Import-Csv ".\test.csv"
+$filehashCount = $filehash.Count
+$csvcount = $readcsv.Count
+if($filehashCount -eq $csvcount)
+{
+    $readcsv|foreach{if($filehash.Contains($_.Path)){if($filehash.Values -eq $_.hash){Write-Host "Success "}else{Write-Host "Failure"}}}
+}
+else
+{
+   
+    foreach($test in $filehash.Keys)
+    {
+        if($test -notin $readcsv.Path)
+        {
+            Write-Host $test
+        }
+    }
+   
+        
+    
+  }
+}
+Get-Fim
